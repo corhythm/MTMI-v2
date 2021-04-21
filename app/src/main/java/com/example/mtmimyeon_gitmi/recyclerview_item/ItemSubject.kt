f206656783aaba3621b12939aba1744ce85e6941
dnr2144 <dnr2144@gmail.com>
2021-04-22 01:49:56 +0900

    과목별 게시판 페이지 추가

--- app/src/main/java/com/example/mtmimyeon_gitmi/recyclerview_item/ItemSubject.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/recyclerview_item/ItemSubject.kt
@@ -1,3 +1,3 @@
 package com.example.mtmimyeon_gitmi.recyclerview_item
 
-data class ItemSubject (val subjectName: String, val subjectCode: String, val subjectTime: String)
+data class ItemSubject (val subjectIdx: Int, val subjectName: String, val subjectCode: String, val subjectTime: String)
