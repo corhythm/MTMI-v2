9a5ab4ab4258f53b1f2c3838b8079448dcaeb412
CHOI97 <chlwjrh@gmail.com>
2021-05-04 19:37:49 +0900

    회원가입 , 로그인 구현 signup activity id 변경필요

---
+++ app/src/main/java/com/example/mtmimyeon_gitmi/DB/Database_M.kt
@@ -0,0 +1,48 @@
+package com.example.mtmimyeon_gitmi.DB
+
+import android.app.Activity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class Database_M {
+    private var firebaseAuth : FirebaseAuth?=null
+
+    fun createEmail(id:String,pw:String,activity:Activity) : Boolean {  // -> 회원가입 메소드
+        var check=true
+        Log.d(" id and password",id+" , "+pw)
+        if(id.length==0||pw.length==0){
+            check=false
+            Toast.makeText(activity,"아이디 혹은 비밀번호를 입력해주세요.",Toast.LENGTH_LONG).show()
+        }else{
+            firebaseAuth!!.createUserWithEmailAndPassword(id,pw)
+                .addOnCompleteListener(activity) {
+                    if (it.isSuccessful){
+                        val user = firebaseAuth?.currentUser
+                        Log.d("createEmail : ", "Sign up Successful")//가입성공
+
+                    } else {
+                        Log.d("createEmail : ", "Sign up Failed")//가입실패
+                        check=false
+                    }
+                }
+        }
+        return check
+    }
+    fun loginEmail(id:String,pw:String,activity:Activity) :Boolean{ // -> 로그인 메소드
+        var check=true
+        firebaseAuth!!.signInWithEmailAndPassword(id,pw)
+            .addOnCompleteListener(activity){
+                if(it.isSuccessful){
+                    Log.d("loginEmail : ","Login Success") //로그인 성공
+                    val user=firebaseAuth?.currentUser
+
+                }
+                else{
+                    Log.d("loginEmail : ","Login Failed") //로그인 실패
+                    check=false
+                }
+            }
+        return check
+    }
+}
--- app/src/main/java/com/example/mtmimyeon_gitmi/account/SignUpActivity.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/account/SignUpActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.example.mtmimyeon_gitmi.DB.Database_M
 import com.example.mtmimyeon_gitmi.R
 import com.example.mtmimyeon_gitmi.databinding.ActivitySignUpBinding
 import com.royrodriguez.transitionbutton.TransitionButton
@@ -15,6 +16,8 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         init()
     }
 
@@ -27,7 +30,13 @@ class SignUpActivity : AppCompatActivity() {
 
             // Do your networking task or background work here.
             Handler().postDelayed(Runnable {
-                val isSuccessful: Boolean = true
+                var DB_M=Database_M()
+
+                Log.d("id check : ",(binding.editTextSignUpID).text.toString())
+                Log.d("pw check : ",binding.editTextSignUpPW.text.toString())
+                var db_id:String=binding.editTextSignUpID.text.toString()
+                var db_pw:String=binding.editTextSignUpPW.text.toString()
+                val isSuccessful: Boolean = DB_M.createEmail(binding.editTextSignUpID.text.toString(),binding.editTextSignUpPW.text.toString(),this)
 
                 // Choose a stop animation if your call was succesful or not
                 if (isSuccessful) {
