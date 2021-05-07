cc5d3e5993303e2183d1312758252731f5b6e1eb
CHOI97 <chlwjrh@gmail.com>
2021-05-08 02:09:03 +0900

    callback 추가사항

--- app/src/main/java/com/example/mtmimyeon_gitmi/DB/Database_M.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/DB/Database_M.kt
@@ -1,13 +1,16 @@
 package com.example.mtmimyeon_gitmi.DB
 
 import android.app.Activity
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.*
 
 class Database_M {
     private var firebaseAuth : FirebaseAuth= FirebaseAuth.getInstance()
 
+<<<<<<< Updated upstream
     fun createEmail(id:String,pw:String,activity:Activity) : Boolean {  // -> 회원가입 메소드
         var check=true
         Log.d(" id and password",id+" , "+pw)
@@ -24,10 +27,32 @@ class Database_M {
                     } else {
                         Log.d("createEmail : ", "Sign up Failed")//가입실패
                         check=false
+=======
+    fun createEmail(id: String, pw: String, activity: Activity,callback: Callback<Boolean>){  // -> 회원가입 메소드
+        Log.d(" id and password", "$id, $pw")
+        runBlocking {
+            if (id.isEmpty() || pw.isEmpty()) {
+                callback.onCallback(false)
+                Toast.makeText(activity, "아이디 혹은 비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
+            } else {
+                Log.d("LOG", "회원가입 실행중입니다.")
+                firebaseAuth.createUserWithEmailAndPassword(id, pw)
+                    .addOnCompleteListener(activity) {
+                        if (it.isSuccessful) {
+                            val user = firebaseAuth.currentUser
+                            callback.onCallback(true)
+                            Log.d("createEmail: ", "Sign up Successful")//가입성공
+                        } else {
+                            Log.d("createEmail: ", "Sign up Failed")//가입실패
+                            callback.onCallback(false)
+                            Toast.makeText(activity, "회원가입 오류", Toast.LENGTH_LONG).show()
+                        }
+>>>>>>> Stashed changes
                     }
-                }
+            }
         }
-        return check
+        Log.d("end if , else", ": 조건문 처리 종료")
+        Log.d("return chekck ", ": 체킹 값 출력")
     }
     fun loginEmail(id:String,pw:String,activity:Activity) :Boolean{ // -> 로그인 메소드
         var check=true
--- app/src/main/java/com/example/mtmimyeon_gitmi/account/SignUpActivity.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/account/SignUpActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.example.mtmimyeon_gitmi.DB.Callback
 import com.example.mtmimyeon_gitmi.DB.Database_M
 import com.example.mtmimyeon_gitmi.R
 import com.example.mtmimyeon_gitmi.databinding.ActivitySignUpBinding
@@ -11,7 +12,6 @@ import com.royrodriguez.transitionbutton.TransitionButton
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -31,7 +31,9 @@ class SignUpActivity : AppCompatActivity() {
             // Do your networking task or background work here.
             Handler().postDelayed(Runnable {
                 var DB_M=Database_M()
+                var isSuccessful:Boolean
 
+<<<<<<< Updated upstream
                 Log.d("id check : ",(binding.editTextSignUpId).text.toString())
                 Log.d("pw check : ",binding.editTextSignUpPw.text.toString())
 
@@ -47,6 +49,36 @@ class SignUpActivity : AppCompatActivity() {
                 } else {
                     binding.buttonSignUpGoToSignUp.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null)
                 }
+=======
+                //임시코드: 로그랑 데이터값 확인용 ,삭제예정
+                Log.d("id check : ", binding.editTextSignUpId.text.toString())
+                Log.d("pw check : ",binding.editTextSignUpPw.text.toString())
+                var db_id:String=binding.editTextSignUpId.text.toString()
+                var db_pw:String=binding.editTextSignUpPw.text.toString()
+
+                // interface로 callback처리함 일단 임시방편용 이후에 문제가 생길경우 다른코드 대안 찾을것.
+                DB_M.createEmail(binding.editTextSignUpId.text.toString(),binding.editTextSignUpPw.text.toString(),this,
+                    object : Callback<Boolean> {
+                        override fun onCallback(data: Boolean) {
+                            isSuccessful=data
+                            Log.d("callback method",": in")
+                            Log.d("boolean check",isSuccessful.toString())
+                            // Choose a stop animation if your call was successful or not
+                            if (isSuccessful) {
+                                binding.buttonSignUpGoToSignUp.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND
+                                ) {
+                                    Log.d("로그", binding.editTextSignUpConfirmPw.text.toString())
+                                    finish()
+                                    Log.d("callback method",": out")
+                                }
+                            } else {
+                                binding.buttonSignUpGoToSignUp.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null)
+                                Log.d("callback method",": create auth faied")
+                            }
+                        }
+                    })
+                Log.d("After callback  : ","Create method exit")
+>>>>>>> Stashed changes
             }, 300)
         }
     }
