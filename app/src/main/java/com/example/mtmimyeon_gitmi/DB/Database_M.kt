a5443314d4a37c2007cb46b7b6d36f80474299c4
CHOI97 <chlwjrh@gmail.com>
2021-05-09 03:52:26 +0900

    회원가입 입력값 체크 , spinner 부분 null체크

--- app/src/main/java/com/example/mtmimyeon_gitmi/DB/Database_M.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/DB/Database_M.kt
@@ -10,39 +10,87 @@ import kotlinx.coroutines.*
 class Database_M {
     private var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
-
+    //  회원가입 메소드 ( 정보 기입 필요한 )
+//    fun createEmail(
+//        id: String,
+//        pw: String,
+//        confirm: String,
+//        name: String,
+//        studentId: String,
+//        major: String,
+//        birth: String,
+//        gender: String,
+//        activity: Activity,
+//        callback: Callback<Boolean>
+//    ) {  // -> 회원가입 메소드
+//        Log.d(" id and password", "$id, $pw")
+//
+//        if (id.isEmpty() || pw.isEmpty() || confirm.isEmpty() || name.isEmpty() || studentId.isEmpty() || major.isEmpty() || birth.isEmpty() || gender.isEmpty()) {
+//            callback.onCallback(false)
+//            Toast.makeText(activity, "아이디 혹은 비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
+//        } else {
+//            Log.d("LOG", "회원가입 실행중입니다.")
+//            firebaseAuth.createUserWithEmailAndPassword(id, pw)
+//                .addOnCompleteListener(activity) {
+//                    if (it.isSuccessful) {
+//                        val user = firebaseAuth.currentUser
+//                        callback.onCallback(true)
+//                        Log.d("createEmail: ", "Sign up Successful")//가입성공
+//                    } else {
+//                        Log.d("createEmail: ", "Sign up Failed")//가입실패
+//                        callback.onCallback(false)
+//                        Toast.makeText(activity, "회원가입 실패: 다시 확인해주세요", Toast.LENGTH_LONG).show()
+//                    }
+//                }
+//        }
+//
+//        Log.d("end if , else", ": 조건문 처리 종료")
+//        Log.d("return chekck ", ": 체킹 값 출력")
+//    }
+    //정보 기입 필요없는 메소드 ( 테스트시 사용)
     fun createEmail(
         id: String,
         pw: String,
+        confirm: String?,
+        name: String?,
+        studentId: String?,
+        major: String?,
+        birth: String?,
+        gender: String?,
         activity: Activity,
         callback: Callback<Boolean>
     ) {  // -> 회원가입 메소드
         Log.d(" id and password", "$id, $pw")
-        runBlocking {
-            if (id.isEmpty() || pw.isEmpty()) {
-                callback.onCallback(false)
-                Toast.makeText(activity, "아이디 혹은 비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
-            } else {
-                Log.d("LOG", "회원가입 실행중입니다.")
-                firebaseAuth.createUserWithEmailAndPassword(id, pw)
-                    .addOnCompleteListener(activity) {
-                        if (it.isSuccessful) {
-                            val user = firebaseAuth.currentUser
-                            callback.onCallback(true)
-                            Log.d("createEmail: ", "Sign up Successful")//가입성공
-                        } else {
-                            Log.d("createEmail: ", "Sign up Failed")//가입실패
-                            callback.onCallback(false)
-                            Toast.makeText(activity, "회원가입 오류", Toast.LENGTH_LONG).show()
-                        }
+
+        if (id.isEmpty() || pw.isEmpty()) {
+            callback.onCallback(false)
+            Toast.makeText(activity, "아이디 혹은 비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
+        } else {
+            Log.d("LOG", "회원가입 실행중입니다.")
+            firebaseAuth.createUserWithEmailAndPassword(id, pw)
+                .addOnCompleteListener(activity) {
+                    if (it.isSuccessful) {
+                        val user = firebaseAuth.currentUser
+                        callback.onCallback(true)
+                        Log.d("createEmail: ", "Sign up Successful")//가입성공
+                    } else {
+                        Log.d("createEmail: ", "Sign up Failed")//가입실패
+                        callback.onCallback(false)
+                        Toast.makeText(activity, "회원가입 실패: 다시 확인해주세요", Toast.LENGTH_LONG).show()
                     }
-            }
+                }
         }
+
         Log.d("end if , else", ": 조건문 처리 종료")
         Log.d("return chekck ", ": 체킹 값 출력")
     }
 
-    fun loginEmail(id: String, pw: String, activity: Activity,callback: Callback<Boolean>){ // -> 로그인 메소드
+    fun loginEmail(
+        id: String,
+        pw: String,
+        activity: Activity,
+        callback: Callback<Boolean>
+    ) { // -> 로그인 메소드
         firebaseAuth!!.signInWithEmailAndPassword(id, pw)
             .addOnCompleteListener(activity) {
                 if (it.isSuccessful) {
@@ -51,6 +99,7 @@ class Database_M {
                     callback.onCallback(true)
                 } else {
                     Log.d("loginEmail : ", "Login Failed") //로그인 실패
+                    Toast.makeText(activity, "아이디와 비밀번호를 확인해주세요.", Toast.LENGTH_LONG).show()
                     callback.onCallback(false)
                 }
             }
--- app/src/main/java/com/example/mtmimyeon_gitmi/account/SignUpActivity.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/account/SignUpActivity.kt
@@ -37,15 +37,35 @@ class SignUpActivity : AppCompatActivity() {
                 //임시코드: 로그랑 데이터값 확인용 ,삭제예정
                 Log.d("id check : ", binding.editTextSignUpId.text.toString())
                 Log.d("pw check : ", binding.editTextSignUpPw.text.toString())
+
+                //유저가 기입한 내용
                 var signUp_userId: String = binding.editTextSignUpId.text.toString()
                 var signUp_userPw: String = binding.editTextSignUpPw.text.toString()
+                var signUp_confirmPw: String = binding.editTextSignUpConfirmPw.text.toString()
                 var signUp_userStudentId: String = binding.editTextSignUpStudentId.text.toString()
                 var signUp_userBirth: String = binding.editTextSignUpDateOfBirth.text.toString()
-                var signUp_userMajor: String = binding.spinnerSignUpMajorList.selectedItem.toString()
-                var signUp_userGender: String = binding.radioButtonSignUpGenderGroup.checkedRadioButtonId.toString()
+                var signUp_userName: String = binding.editTextSignUpName.text.toString()
+
+                // 전공 선택시 null 체크
+                var signUp_userMajor: String?
+                if(binding.spinnerSignUpMajorList.selectedItem!=null) {
+                    signUp_userMajor =
+                        binding.spinnerSignUpMajorList.selectedItem.toString()
+                }else{
+                    signUp_userMajor=null
+                }
+                var signUp_userGender: String =
+                    binding.radioButtonSignUpGenderGroup.checkedRadioButtonId.toString()
 
                 // interface로 callback처리함 일단 임시방편용 이후에 문제가 생길경우 다른코드 대안 찾을것.
-                DB_M.createEmail(signUp_userId,signUp_userPw,
+                DB_M.createEmail(signUp_userId,
+                    signUp_userPw,
+                    signUp_confirmPw,
+                    signUp_userName,
+                    signUp_userStudentId,
+                    signUp_userMajor,
+                    signUp_userBirth,
+                    signUp_userGender,
                     this,
                     object : Callback<Boolean> {
                         override fun onCallback(data: Boolean) {
