928eef096e131d73958f100fceef9314be1d0078
dnr2144 <dnr2144@gmail.com>
2021-05-09 11:47:55 +0900

    시간표 추가 코드 리팩토링

--- app/src/main/java/com/example/mtmimyeon_gitmi/myclass/MyClassTimeTableActivity.kt
+++ app/src/main/java/com/example/mtmimyeon_gitmi/myclass/MyClassTimeTableActivity.kt
@@ -48,59 +48,45 @@ class MyClassTimeTableActivity : AppCompatActivity() {
             SharedPrefManager.getUserLmsSubjectInfoList() as ArrayList<ItemSubjectInfo>
 
         try {
-            // subject timetable에 추가
+            // 현재 수강중인 과목 시간표에 추가
             subjectInfoList.forEach {
-                if (it.lectureTime.contains(',')) { // 강의 시간대가 두 개면
-                    it.lectureTime.split(',')[0].subSequence(2, 4).toString()
-                    schedules.add(Schedule().apply {
-                        // 수(1000-1050),금(0900-1050) 문자열 자르기(코드가 상당히 더럽고, 에러 발생 확률이 높다)
-                        classTitle = it.subjectName.split('(')[0] // 공학수학1(KME02103-0281)
-                        classPlace = it.professor
-//                        professorName = it.professor
-                        day = it.lectureTime.split(',')[0].subSequence(0, 1).toString().getDay()
-                        startTime = Time(
-                            it.lectureTime.split(',')[0].subSequence(2, 4).toString().toInt(),
-                            it.lectureTime.split(',')[0].subSequence(4, 6).toString().toInt()
-                        )
-                        endTime = Time(
-                            it.lectureTime.split(',')[0].subSequence(7, 9).toString().toInt(),
-                            it.lectureTime.split(',')[0].subSequence(9, 11).toString().toInt()
-                        )
-                        Log.d(TAG, it.lectureTime.split(',')[0].subSequence(0, 1).toString())
-                    })
+                val title = it.subjectName.split('(')[0] // 공학수학1(KME02103-0281)
+                val professor = it.professor
+                var lectureDay = it.lectureTime.subSequence(0, 1).toString().getDay()
+                var lectureStartTime = Time(
+                    it.lectureTime.subSequence(2, 4).toString().toInt(),
+                    it.lectureTime.subSequence(4, 6).toString().toInt()
+                )
+                var lectureEndTime = Time(
+                    it.lectureTime.subSequence(7, 9).toString().toInt(),
+                    it.lectureTime.subSequence(9, 11).toString().toInt()
+                )
+                schedules.add(Schedule().apply {
+                    classTitle = title
+                    classPlace = professor
+                    day = lectureDay
+                    startTime = lectureStartTime
+                    endTime = lectureEndTime
+                })
 
+                if (it.lectureTime.contains(',')) { // 강의 시간대가 두 개면
+                    lectureDay = it.lectureTime.split(',')[1].subSequence(0, 1).toString().getDay()
+                    lectureStartTime = Time(
+                        it.lectureTime.split(',')[1].subSequence(2, 4).toString().toInt(),
+                        it.lectureTime.split(',')[1].subSequence(4, 6).toString().toInt()
+                    )
+                    lectureEndTime = Time(
+                        it.lectureTime.split(',')[1].subSequence(7, 9).toString().toInt(),
+                        it.lectureTime.split(',')[1].subSequence(9, 11).toString().toInt()
+                    )
                     schedules.add(Schedule().apply {
-                        // 수(1000-1050),금(0900-1050) 문자열 자르기(코드가 상당히 더럽고, 에러 발생 확률이 높다)
-                        classTitle = it.subjectName.split('(')[0] // 공학수학1(KME02103-0281)
-                        classPlace = it.professor
-//                        professorName = it.professor
-                        day = it.lectureTime.split(',')[1].subSequence(0, 1).toString().getDay()
-                        startTime = Time(
-                            it.lectureTime.split(',')[1].subSequence(2, 4).toString().toInt(),
-                            it.lectureTime.split(',')[1].subSequence(4, 6).toString().toInt()
-                        )
-                        endTime = Time(
-                            it.lectureTime.split(',')[1].subSequence(7, 9).toString().toInt(),
-                            it.lectureTime.split(',')[1].subSequence(9, 11).toString().toInt()
-                        )
-                        Log.d(TAG, it.lectureTime.split(',')[1].subSequence(0, 1).toString())
-                    })
-                } else { // 강의 시간대가 1개일 때
-                    schedules.add(Schedule().apply {
-                        classTitle = it.subjectName.split('(')[0]
-                        classPlace = it.professor
-                        day = it.lectureTime.subSequence(0, 1).toString().getDay()
-                        startTime = Time(
-                            it.lectureTime.subSequence(2, 4).toString().toInt(),
-                            it.lectureTime.subSequence(4, 6).toString().toInt()
-                        )
-                        endTime = Time(
-                            it.lectureTime.subSequence(7, 9).toString().toInt(),
-                            it.lectureTime.subSequence(9, 11).toString().toInt()
-                        )
+                        classTitle = title
+                        classPlace = professor
+                        day = lectureDay
+                        startTime = lectureStartTime
+                        endTime = lectureEndTime
                     })
                 }
-
             }
         } catch (exception: Exception) {
             MotionToast.createColorToast(
@@ -114,51 +100,6 @@ class MyClassTimeTableActivity : AppCompatActivity() {
             )
             schedules.clear()
         }
-
-        // test code
-//        var schedule = Schedule()
-//        schedule.classTitle = "Data Structure"; // sets subject
-//        schedule.classPlace = "5401"; // sets place
-//        schedule.professorName = "류연승"; // sets professor
-//        schedule.startTime = Time(10, 0); // sets the beginning of class time (hour,minute)
-//        schedule.endTime = Time(13, 30); // sets the end of class time (hour,minute)
-//        schedules.add(schedule);
-//
-//        schedule = Schedule()
-//        schedule.classTitle = "시스템클라우드보안"; // sets subject
-//        schedule.classPlace = "5407"; // sets place
-//        schedule.professorName = "조민경"; // sets professor
-//        schedule.day = 2
-//        schedule.startTime = Time(14, 0); // sets the beginning of class time (hour,minute)
-//        schedule.endTime = Time(16, 50); // sets the end of class time (hour,minute)
-//        schedules.add(schedule);
-//
-//        schedule = Schedule()
-//        schedule.classTitle = "고급객체지향프로그래밍"; // sets subject
-//        schedule.classPlace = "5401"; // sets place
-//        schedule.professorName = "조세형"; // sets professor
-//        schedule.day = 3
-//        schedule.startTime = Time(9, 0); // sets the beginning of class time (hour,minute)
-//        schedule.endTime = Time(10, 50); // sets the end of class time (hour,minute)
-//        schedules.add(schedule);
-//
-//        schedule = Schedule()
-//        schedule.classTitle = "Algorithm"; // sets subject
-//        schedule.classPlace = "5403"; // sets place
-//        schedule.professorName = "주우석"; // sets professor
-//        schedule.day = 4
-//        schedule.startTime = Time(11, 0); // sets the beginning of class time (hour,minute)
-//        schedule.endTime = Time(3, 50); // sets the end of class time (hour,minute)
-//        schedules.add(schedule);
-//
-//        schedule = Schedule()
-//        schedule.classTitle = "공학수학1"; // sets subject
-//        schedule.classPlace = "h1021"; // sets place
-//        schedule.professorName = "소순태"; // sets professor
-//        schedule.day = 3
-//        schedule.startTime = Time(11, 0); // sets the beginning of class time (hour,minute)
-//        schedule.endTime = Time(3, 50); // sets the end of class time (hour,minute)
-//        schedules.add(schedule);
 
 
         //.. add one or more schedules
